ba2f8a3966802619555e216514c93baf63a21d2c
JBthePenguin <jblab976@gmail.com>
2020-04-16 15:36:28 +0200

    ok to fake role, begin to fake channels

--- my_bot/info/test/fakers.py
+++ my_bot/info/test/fakers.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock
-from discord import Guild, User, Member
+from discord import Guild, User, Member, Role, TextChannel
+from discord.enums import ChannelType
 from discord.ext.commands import Context
 import itertools
 
@@ -8,9 +9,11 @@ discord_id = itertools.count(0)
 
 
 class FakeUser(User):
-    """ Mock for a user """
+    """ Class to fake a user """
 
     def __init__(self, name):
+        """ init a user with id, name, discriminator, avatar
+        and MagicMock for state """
         user_data = {
             'id': next(discord_id), 'username': name,
             'discriminator': name, 'avatar': str(discord_id)}
@@ -18,46 +21,98 @@ class FakeUser(User):
 
 
 class FakeMember(Member):
-    """ Mock for a member """
+    """ Class to fake a member """
 
-    def __init__(self, user, guild):
-        mem_data = {'user': user, 'roles': []}
-        mem_guild = guild
-        super().__init__(data=mem_data, guild=mem_guild, state=MagicMock())
+    def __init__(self, user, guild, roles):
+        """ init a member with a user, a list of roles (id), a guild
+        and MagicMock for state """
+        mem_data = {'user': user, 'roles': roles}
+        super().__init__(data=mem_data, guild=guild, state=MagicMock())
 
     def name_id_update(self, user):
+        " update id, name, avatar, discriminator using user's properties"
         u = self._user
         modified = (user.id, user.name, user.avatar, user.discriminator)
         u.id, u.name, u.avatar, u.discriminator = modified
 
 
-class FakeGuild(Guild):
-    """ Mock for a guild """
+class FakeRole(Role):
+    """ Class to fake a role """
 
-    def __init__(self, name, members, owner_id):
-        super().__init__(data={
+    def __init__(self, id, name, guild, position):
+        """ init a role with id, name, guild, position
+        and MagicMock for state """
+        role_data = {'id': id, 'name': name, 'position': position}
+        super().__init__(data=role_data, guild=guild, state=MagicMock())
+
+
+class FakeTextChannel(TextChannel):
+    """ Class to fake a text channel """
+
+    def __init__(self, name, guild, type, position):
+        """ init a role with a user, a list of roles, a guild
+        and MagicMock for state """
+        channel_data = {
             'id': next(discord_id), 'name': name,
-            'members': members, 'owner_id': owner_id}, state=MagicMock())
+            'type': type, 'position': position}
+        super().__init__(data=channel_data, guild=guild, state=MagicMock())
+
+
+class FakeGuild(Guild):
+    """ Class to fake a guild """
+
+    def __init__(self, name, members, roles, channels, owner_id):
+        """ init a guild with id, name,
+        list of tuple for members (FakeUser, roles(list of name)),
+        list of tuple for roles (name, position), a owner id
+        and MagicMock for state """
+        guild_data = {
+            'id': next(discord_id), 'name': name, 'members': members,
+            'channels': channels, 'roles': roles, 'owner_id': owner_id}
+        super().__init__(data=guild_data, state=MagicMock())
 
     def _from_data(self, guild):
-        # according to Stan, this is always available even
-        # if the guild is unavailable
-        # I don't have this guarantee when someone updates the guild.
-        self.name = guild.get('name')
+        """ override _from_data method to init just necessary properties
+        -id -name -description -roles -members -owner -emojis -channels """
         self.id = int(guild['id'])
-        self._roles = {}
-        # for r in guild.get('roles', []):
-        #     role = Role(guild=self, data=r, state=state)
-        #     self._roles[role.id] = role
-        self.emojis = guild.get('emojis', [])
+        self.name = guild.get('name')
         self.description = guild.get('description')
-        # add members
-        for mdata in guild.get('members', []):
-            member = FakeMember(user=mdata, guild=self)
-            member.name_id_update(mdata)
+        self._roles = {}
+        # add default role @everyone with the same id than the guild
+        default_role = FakeRole(
+            id=self.id, name='@everyone', guild=self, position=0)
+        self._roles[default_role.id] = default_role
+        # add roles
+        name_id_roles = {}
+        for name_pos in guild.get('roles', []):
+            role = FakeRole(
+                id=next(discord_id), name=name_pos[0], guild=self,
+                position=name_pos[1])
+            self._roles[role.id] = role
+            name_id_roles[role.name] = role.id
+        # add members with roles
+        for user_roles in guild.get('members', []):
+            m_roles = []
+            for role_name in user_roles[1]:
+                m_roles.append(name_id_roles[role_name])
+            member = FakeMember(user=user_roles[0], guild=self, roles=m_roles)
+            member.name_id_update(user_roles[0])
             self._add_member(member)
         # add owner
         self.owner_id = guild.get('owner_id')
+        self.emojis = guild.get('emojis', [])
+        # add Channels:
+        for name_type_pos in guild.get('channels', []):
+            if name_type_pos[1] == 'TextChannel':
+                self._add_channel(FakeTextChannel(
+                    guild=self, name=name_type_pos[0],
+                    type=0, position=name_type_pos[2]))
+            # elif c_type == ChannelType.voice.value:
+            #     self._add_channel(VoiceChannel(guild=self, data=c, state=self._state))
+            # elif c_type == ChannelType.category.value:
+            #     self._add_channel(CategoryChannel(guild=self, data=c, state=self._state))
+            # elif c_type == ChannelType.store.value:
+            #     self._add_channel(StoreChannel(guild=self, data=c, state=self._state))
 
 
 class FakeContext(MagicMock):
--- my_bot/info/test/t_shaping.py
+++ my_bot/info/test/t_shaping.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-from datetime import datetime
-from discord import Embed, Guild
+from discord import Embed
 from .fakers import FakeUser, FakeGuild, FakeMember
 from .results import GuildEmbedTestResult
 from ..shaping import GuildEmbed
@@ -37,42 +36,28 @@ class GuildEmbedTest(TestCase):
     def test_add_title_stats(self):
         """ assert after add_title_stats if fields are added
         with the good title and the corresponding number of ojs for value """
-        # fake_guild = FakeGuild(name="Fake guild", owner=FakeUser(name='Joe'), members=[FakeUser(name='Bill'), FakeUser(name='Jean-Pierre')])
-        # fake_owner = FakeMember(user=FakeUser(name='Joe'), guild=fake_guild)
-        # fake_guild.add_owner(fake_owner)
-        # fake_member = FakeMember(user=FakeUser(name='Bill'), guild=fake_guild)
-        # nfake_member = FakeMember(user=FakeUser(name='Jean-Pierre'), guild=fake_guild)
-        # print(fake_member.id)
-        # print(fake_member.name)
-        # print(fake_guild.members)
-        # fake_guild.add_owner(fake_owner)
-        # print(fake_guild.owner.name)
-        # print(fake_guild.owner_id)
-        user_one = FakeUser(name='Bill')
-        print(user_one.name)
-        print(user_one.id)
-        user_two = FakeUser(name='Jean-Pierre')
-        print(user_two.name)
-        print(user_two.id)
-        fake_guild = FakeGuild(name="Fake guild", members=[user_one, user_two], owner_id=0)
-        # print(fake_guild.name)
-        # print(fake_guild._members)
-        # print(fake_guild.members)
+        owner = FakeUser(name='Bill')
+        roles = [('admin', 2), ('master', 1)]
+        members = [
+            (owner, ['admin', 'master']), (FakeUser(name='Joe'), ['master']),
+            (FakeUser(name='Al'), ['master']), (FakeUser(name='John'), []),
+            (FakeUser(name='Jean-Pierre'), []), ]
+        channels = [('saloon', 'TextChannel', 0)]
+        fake_guild = FakeGuild(
+            name="Fake guild", members=members, roles=roles,
+            channels=channels, owner_id=0)
+        print('\n')
+        print(fake_guild.id, fake_guild.name)
+        print('roles:', fake_guild.roles)
+        print(fake_guild.default_role)
         for member in fake_guild.members:
-            print(member.id)
-            print(member.name)
-        # # print(fake_guild.get_member(0))
-        # self.assertIsInstance(fake_guild, Guild)
-        # fake_member = FakeMember(joined_at=str(datetime.now), guild=fake_guild)
-        # owner = FakeMember({'user': 'lemon', 'roles': [1]}, fake_guild)
-        # fake_guild.owner = owner
-        # print(fake_guild.name)
-        print(fake_guild.owner.name)
-        embed = GuildEmbed(self.embed_name, self.embed_icon_url)
-        embed.add_title_stats(fake_guild)
-        print(embed.to_dict())
-        # embed.add_stat(title_mem, ['a', 'b', 'c', 'd', 'e'])
-        # self.assertDictEqual(embed.to_dict(), self.result.add_stat)
+            print(member.id, member.name, member.roles)
+        print(fake_guild.channels[0].type)
+        # print('Owner:', fake_guild.owner.name)
+        # embed = GuildEmbed(self.embed_name, self.embed_icon_url)
+        # embed.add_title_stats(fake_guild)
+        # print(embed.to_dict())
+
 
 
 # class MyTest(aiounittest.AsyncTestCase):
