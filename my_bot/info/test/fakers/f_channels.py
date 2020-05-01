a2f7dd0dd8228fd9d5c44a3b33f91b712cacef0f
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-01 15:03:43 +0000

    change channels fakers

--- my_bot/info/test/fakers/f_channels.py
+++ my_bot/info/test/fakers/f_channels.py
@@ -7,7 +7,7 @@ from .f_permover import FakePermissionOverwrites
 class FakeChannelData(dict):
     """Class to fake a channel data dict."""
 
-    def __init__(self, parent_id, name, position, roles, members):
+    def __init__(self, parent_id, name, position, roles, members, c_type=None):
         """Init with keys id, name, position
         and permission_overwrites if there is role or member."""
         super().__init__()
@@ -18,46 +18,54 @@ class FakeChannelData(dict):
         if roles or members:
             self['permission_overwrites'] = FakePermissionOverwrites(
                 roles, members)
+        if c_type is not None:
+            self['type'] = c_type
 
 
 class FakeCategoryChannel(CategoryChannel):
     """Class to fake a categry channel."""
 
-    def __init__(self, parent_id, name, guild, position, roles, members):
+    def __init__(self, **kwargs):
         """Init with a FakeChannelData, a guild and MagicMock for state."""
-        channel_data = FakeChannelData(
-            parent_id, name, position, roles, members)
-        super().__init__(data=channel_data, guild=guild, state=MagicMock())
+        super().__init__(
+            data=FakeChannelData(
+                kwargs['parent_id'], kwargs['name'], kwargs['position'],
+                kwargs['roles'], kwargs['members']),
+            guild=kwargs['guild'], state=MagicMock())
 
 
 class FakeTextChannel(TextChannel):
     """Class to fake a text channel."""
 
-    def __init__(
-            self, parent_id, name, guild, c_type, position, roles, members):
+    def __init__(self, **kwargs):
         """Init with a FakeChannelData (add 'type' = (text->0 news->5)),
         a guild, and MagicMock for state"""
-        channel_data = FakeChannelData(
-            parent_id, name, position, roles, members)
-        channel_data['type'] = c_type
-        super().__init__(data=channel_data, guild=guild, state=MagicMock())
+        super().__init__(
+            data=FakeChannelData(
+                kwargs['parent_id'], kwargs['name'], kwargs['position'],
+                kwargs['roles'], kwargs['members'], kwargs['c_type']),
+            guild=kwargs['guild'], state=MagicMock())
 
 
 class FakeVoiceChannel(VoiceChannel):
     """Class to fake a voice channel."""
 
-    def __init__(self, parent_id, name, guild, position, roles, members):
+    def __init__(self, **kwargs):
         """Init with a FakeChannelData, a guild and MagicMock for state."""
-        channel_data = FakeChannelData(
-            parent_id, name, position, roles, members)
-        super().__init__(data=channel_data, guild=guild, state=MagicMock())
+        super().__init__(
+            data=FakeChannelData(
+                kwargs['parent_id'], kwargs['name'], kwargs['position'],
+                kwargs['roles'], kwargs['members']),
+            guild=kwargs['guild'], state=MagicMock())
 
 
 class FakeStoreChannel(StoreChannel):
     """Class to fake a store channel."""
 
-    def __init__(self, parent_id, name, guild, position, roles, members):
+    def __init__(self, **kwargs):
         """Init with a FakeChannelData, a guild and MagicMock for state."""
-        channel_data = FakeChannelData(
-            parent_id, name, position, roles, members)
-        super().__init__(data=channel_data, guild=guild, state=MagicMock())
+        super().__init__(
+            data=FakeChannelData(
+                kwargs['parent_id'], kwargs['name'], kwargs['position'],
+                kwargs['roles'], kwargs['members']),
+            guild=kwargs['guild'], state=MagicMock())
