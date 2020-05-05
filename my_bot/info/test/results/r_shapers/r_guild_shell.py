b43949782ca7b8c28ce99cb744c0f13945eb32bf
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-05 12:47:48 +0000

    test commit, pb to stage r_guild_shell.py 2

--- my_bot/info/test/results/r_shapers/r_guild_shell.py
+++ my_bot/info/test/results/r_shapers/r_guild_shell.py
@@ -4,22 +4,3 @@ G_SHELL_RESULTS = {}
 
 class GuildShellTestResult():
     """Class with GuildShellTest expected result."""
-
-    @property
-    def add_list(self):
-        """Return the result expected for test_add_list."""
-        return {
-            'guild': '\n'.join([
-                '\n##########  Guild ##########',
-                '- 6 - Full guild\n']),
-            'owner': '\n'.join([
-                '\n##########  Owner ##########',
-                '- 0 - Jean-Pierre\n']),
-            'objs': '\n'.join([
-                '\n########## 6 Members ##########',
-                '- 1 - Al',
-                '- 3 - Billy',
-                '- 0 - Jean-Pierre',
-                '- 2 - Joe',
-                '- 4 - John',
-                '- 5 - Mike\n'])}
