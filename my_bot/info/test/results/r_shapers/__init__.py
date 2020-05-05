0bd634530432188b60d0f4807163b3cebc4d2999
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-05 12:58:26 +0000

    pb to stage when dict name is G_SHELL_RESULTS, but works with G_SHL_RESULTS -> why ?????

--- my_bot/info/test/results/r_shapers/__init__.py
+++ my_bot/info/test/results/r_shapers/__init__.py
@@ -1,5 +1,5 @@
 from .r_guild_embed import G_EMBED_RESULTS
-from .r_guild_shell import G_SHELL_RESULTS
+from .r_guild_shell import G_SHL_RESULTS
 from .r_comp_embed import ComponentEmbedTestResult
 
-__all__ = ['G_EMBED_RESULTS', 'G_SHELL_RESULTS', 'ComponentEmbedTestResult']
+__all__ = ['G_EMBED_RESULTS', 'G_SHL_RESULTS', 'ComponentEmbedTestResult']
