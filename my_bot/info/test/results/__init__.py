b289b6a0fe2210e9308db327298a632c069842ad
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-05 14:53:16 +0000

    ok for big clean in all tests and results, next add method to GuildEmbed

--- my_bot/info/test/results/__init__.py
+++ my_bot/info/test/results/__init__.py
@@ -1,9 +1,8 @@
+from .r_shapers import G_EMBED_RESULTS, G_SHL_RESULTS, C_EMBED_RESULTS
 from .r_guild import G_RESULTS
 from .r_components import C_RESULTS
 from .r_config import CONF_RESULTS
-from .r_shapers import (
-    G_EMBED_RESULTS, ComponentEmbedTestResult)
 
 __all__ = [
-    "G_RESULTS", 'C_RESULTS', 'G_EMBED_RESULTS', 'CONF_RESULTS',
-    "ComponentEmbedTestResult"]
+    'G_EMBED_RESULTS', 'G_SHL_RESULTS', 'C_EMBED_RESULTS',
+    "G_RESULTS", 'C_RESULTS', 'CONF_RESULTS']
--- my_bot/info/test/results/r_shapers/__init__.py
+++ my_bot/info/test/results/r_shapers/__init__.py
@@ -1,5 +1,5 @@
 from .r_guild_embed import G_EMBED_RESULTS
 from .r_guild_shell import G_SHL_RESULTS
-from .r_comp_embed import ComponentEmbedTestResult
+from .r_comp_embed import C_EMBED_RESULTS
 
-__all__ = ['G_EMBED_RESULTS', 'G_SHL_RESULTS', 'ComponentEmbedTestResult']
+__all__ = ['G_EMBED_RESULTS', 'G_SHL_RESULTS', 'C_EMBED_RESULTS']
