00a26a0435636ddc0186597653d26f56ba9a091a
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-07 19:07:25 +0000

    ok to clean run_test, next customize html report template

--- run_test.py
+++ run_test.py
@@ -6,20 +6,15 @@ from argparse import (
     ArgumentParser, HelpFormatter, SUPPRESS, OPTIONAL, ZERO_OR_MORE,
     ONE_OR_MORE, REMAINDER, PARSER)
 
-I_TEST_NAMES = [t.__name__ for t in INFO_TESTS]
 EVENT_TESTS = []
-E_TEST_NAMES = [t.__name__ for t in EVENT_TESTS]
-TESTS = {
-    'info': INFO_TESTS,
-    'event': EVENT_TESTS}
-TEST_NAMES = {
-    'info': I_TEST_NAMES,
-    'event': E_TEST_NAMES}
+ALL_TESTS = EVENT_TESTS + INFO_TESTS
 
 
 class MyHelpFormatter(HelpFormatter):
+    """Overide HelpFormatter to customize help message."""
 
     def _format_args(self, action, default_metavar):
+        """Just change result for args."""
         get_metavar = self._metavar_formatter(action, default_metavar)
         if action.nargs is None:
             result = '%s' % get_metavar(1)
@@ -49,6 +44,7 @@ class MyHelpFormatter(HelpFormatter):
         return result
 
     def _format_action_invocation(self, action):
+        """Just add a \n between args."""
         if not action.option_strings:
             default = self._get_default_metavar_for_positional(action)
             metavar, = self._metavar_formatter(action, default)(1)
@@ -63,86 +59,86 @@ class MyHelpFormatter(HelpFormatter):
                 for option_string in action.option_strings:
                     parts.append('%s %s' % (option_string, args_string))
             return '\n' + ', '.join(parts)
+            # return ', '.join(parts)
 
 
-class MyTestRunner(HTMLTestRunner):
+class TestArgumentParser(ArgumentParser):
+    """Override ArgumentParser..."""
 
-    pass
-
-
-def get_suite(tests):
-    suite_list = []
-    for test_case in tests:
-        suite_list.append(
-            TestLoader().loadTestsFromTestCase(test_case))
-    return TestSuite(suite_list)
-
-
-if __name__ == "__main__":
-    if len(argv) == 1:  # no argument passed
-        MyTestRunner(
-            output='html_test_reports', combine_reports=True,
-            report_name='full_test', add_timestamp=False).run(
-                get_suite(INFO_TESTS + EVENT_TESTS))
-    else:
-        # check if all argv exist
-        parser = ArgumentParser(
+    def __init__(self, groups):
+        """Init ArgumentParser with MyHelpFormatter and description.
+        groups -> [(name, [TestCase, ...]), (..)].
+        Name is used for arg's name to run all tests."""
+        super().__init__(
             formatter_class=MyHelpFormatter, description='\n'.join([
                 'Without argument to run all tests, ',
                 'or with optionnal one(s) without option to run all ',
                 "specific app or TestCase tests, or with test's names",
                 " in option for a TestCase arg to run specific tests.", ]))
-        parser._optionals.title = 'Help'
-        # argument groups
-        for arg_name in ['info', 'event']:
-            group = parser.add_argument_group(f"{arg_name.title()} Tests")
-            # app arg
+        # groups
+        self._optionals.title = 'Help'
+        for name_tests in groups:
+            group = self.add_argument_group(f"{name_tests[0].title()} Tests")
+            # all tests arg
             group.add_argument(
-                f"--{arg_name}", action='store_true',
-                help=f"Run all {arg_name.title()} tests.")
-            for test_case in TESTS[arg_name]:
-                # test case arg
-                for test_name in TEST_NAMES[arg_name]:
-                    if test_name == test_case.__name__:
-                        break
+                f"--{name_tests[0]}", action='store_true',
+                help=f"Run all {name_tests[0].title()} tests.")
+            for test_case in name_tests[1]:
+                test_name = test_case.__name__
                 # optionnal choise -> tests case methods
                 methods = TestLoader().loadTestsFromTestCase(test_case)._tests
                 methods_names = [m._testMethodName for m in methods]
+                # test ase tests
                 group.add_argument(
                     f"--{test_name}", choices=methods_names, nargs='*',
                     help="".join([
                         f"Without option to run all {test_name} tests,",
                         " or pass method's names to run specific tests. ",
                         "Allowed values are: ", " - ".join(methods_names)]))
+
+
+class MyTestRunner(HTMLTestRunner):
+    """Override HTMLTestRunner..."""
+
+    def __init__(self, report_name):
+        super().__init__(
+            output='html_test_reports', combine_reports=True,
+            report_name=report_name, add_timestamp=False)
+
+
+def get_suite(tests, options=None):
+    """Return the corresponding tests suite"""
+    suite_list = []
+    if options is None:
+        for test_case in tests:
+            suite_list.append(
+                TestLoader().loadTestsFromTestCase(test_case))
+    else:
+        test_case = tests[0]
+        for option in options:
+            suite_list.append(test_case(option))
+    return TestSuite(suite_list)
+
+
+if __name__ == "__main__":
+    if len(argv) == 1:  # no argument passed
+        MyTestRunner('full_test').run(get_suite(ALL_TESTS))
+    else:  # parse args
+        groups = [('info', INFO_TESTS), ('event', EVENT_TESTS)]
+        parser = TestArgumentParser(groups)
         args = parser.parse_args()
-        if args.info:  # all info tests
-            MyTestRunner(
-                output='html_test_reports', combine_reports=True,
-                report_name='info_test', add_timestamp=False).run(
-                    get_suite(INFO_TESTS))
-        if args.event:  # all event test
-            MyTestRunner(
-                output='html_test_reports', combine_reports=True,
-                report_name='event_test', add_timestamp=False).run(
-                    get_suite(EVENT_TESTS))
+        # check args
         args_dict = vars(args)
-        for test_name in (I_TEST_NAMES + E_TEST_NAMES):
+        for name_tests in groups:
+            if args_dict[name_tests[0]]:  # all group test
+                MyTestRunner(f"{name_tests[0]}_test").run(
+                    get_suite(name_tests[1]))
+        for test_case in ALL_TESTS:
+            test_name = test_case.__name__
             options = args_dict[test_name]
             if isinstance(options, list):
-                for test_case in (INFO_TESTS + EVENT_TESTS):
-                    if test_name == test_case.__name__:
-                        break
                 if not options:  # all test case tests
-                    MyTestRunner(
-                        output='html_test_reports', combine_reports=True,
-                        report_name=test_name, add_timestamp=False).run(
-                            get_suite([test_case]))
-                else:
-                    print(options)
-                    suite_list = []
-                    for option in options:
-                        suite_list.append(test_case(option))
-                    MyTestRunner(
-                        output='html_test_reports', combine_reports=True,
-                        report_name=test_name + '_methods', add_timestamp=False).run(
-                            TestSuite(suite_list))
+                    MyTestRunner(test_name).run(get_suite([test_case]))
+                else:  # tests case methods
+                    MyTestRunner(f"{test_name}_methods").run(
+                        get_suite([test_case], options))
