7c08ac9e3f32d65836385acf4b4f0d84b1d5b5d0
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-11 12:16:54 +0000

    ok for clean com_parser.py, next runner.py

--- my_tester/com_parser.py
+++ my_tester/com_parser.py
@@ -1,46 +1,18 @@
 import sys
+from argparse import ArgumentParser, HelpFormatter
 from unittest import TestLoader
-from argparse import (
-    ArgumentParser, HelpFormatter, SUPPRESS, OPTIONAL, ZERO_OR_MORE,
-    ONE_OR_MORE, REMAINDER, PARSER)
 from .runner import TestCaseRunner
 
 
 class HelpFormatterTestCase(HelpFormatter):
-    """Overide HelpFormatter to customize help message."""
+    """Override HelpFormatter to customize help message."""
 
     def _format_args(self, action, default_metavar):
-        """Just change result for args."""
-        get_metavar = self._metavar_formatter(action, default_metavar)
-        if action.nargs is None:
-            result = '%s' % get_metavar(1)
-        elif action.nargs == OPTIONAL:
-            result = '[%s]' % get_metavar(1)
-        elif action.nargs == ZERO_OR_MORE:
-            metavar = get_metavar(1)
-            if len(metavar) == 2:
-                result = '[%s [%s ...]]' % metavar
-            else:
-                result = "[Optionnal: method's names]"
-                # result = '%s' % metavar
-        elif action.nargs == ONE_OR_MORE:
-            result = '%s [%s ...]' % get_metavar(2)
-        elif action.nargs == REMAINDER:
-            result = '...'
-        elif action.nargs == PARSER:
-            result = '%s ...' % get_metavar(1)
-        elif action.nargs == SUPPRESS:
-            result = ''
-        else:
-            try:
-                formats = ['%s' for _ in range(action.nargs)]
-            except TypeError:
-                raise ValueError("invalid nargs value") from None
-            result = ' '.join(formats) % get_metavar(action.nargs)
-        return result
+        """Return the result to display nargs(ZERO_OR_MORE)."""
+        return "options"
 
     def _format_action_invocation(self, action):
-        """Just add a \n between args."""
+        """Add \n a the end of original method to add a space between args."""
         if not action.option_strings:
             default = self._get_default_metavar_for_positional(action)
             metavar, = self._metavar_formatter(action, default)(1)
@@ -55,7 +27,6 @@ class HelpFormatterTestCase(HelpFormatter):
                 for option_string in action.option_strings:
                     parts.append('%s %s' % (option_string, args_string))
             return '\n' + ', '.join(parts)
-            # return ', '.join(parts)
 
 
 class ArgumentParserTestCase(ArgumentParser):
@@ -70,14 +41,14 @@ class ArgumentParserTestCase(ArgumentParser):
         self.arg_groups = arg_groups
         self.all_tests = []
         super().__init__(  # init ArgumentParser
-            formatter_class=HelpFormatterTestCase, description='\n'.join([
-                'Without argument to run all tests, ',
-                'or with optionnal one(s) without option to run ',
-                "specific app or TestCase tests, or with method's names",
-                " in options for TestCase arg to run specific tests."]))
+            formatter_class=HelpFormatterTestCase, description=''.join([
+                'Without argument to run all tests, or with optionnal ',
+                'one(s) without option to run group or TestCase tests, or ',
+                'with method names in options to a TestCase arg to run',
+                'specific test methods.']))
         self._optionals.title = 'Help'  # help msg's title for help command
         self.make_arg_groups()  # groups
-        self.check_and_run()
+        self.check_and_run()  # check args and run the corresponding tests
 
     def make_arg_groups(self):
         """ For each group, set help message's title with his name,
@@ -87,37 +58,34 @@ class ArgumentParserTestCase(ArgumentParser):
         ***construct all tests list by adding each tests cases list"""
         for group_name, tests_cases in self.arg_groups:  # groups
             self.all_tests += tests_cases
-            group = self.add_argument_group(f"{group_name.title()} Tests")
+            group = self.add_argument_group(f"{group_name.title()} TestCases")
             group.add_argument(  # arg with group name to run all tests
                 f"--{group_name}", action='store_true',
                 help=f"Run all {group_name.title()} tests.")
-            for test_case in tests_cases:
-                methods = TestLoader().loadTestsFromTestCase(test_case)._tests
-                methods_names = [m._testMethodName for m in methods]
-                # arg with testcase name and methods names for optionnal params
+            for t_case in tests_cases:
+                methods = TestLoader().loadTestsFromTestCase(t_case)._tests
+                m_names = [m._testMethodName for m in methods]
                 group.add_argument(  # arg with testcase name
-                    f"--{test_case.__name__}", choices=methods_names,
-                    nargs='*', help="".join([  # optionnal params with methods
-                        f"Without option to run {test_case.__name__}'s tests,",
-                        " or pass method's names to run specific tests. ",
-                        "Allowed values are: ", " - ".join(methods_names)]))
+                    f"--{t_case.__name__}", help=' - '.join(m_names),
+                    nargs='*', choices=m_names)  # methods names for params
 
     def check_and_run(self):
-        """Check args and set the corresponding tests cases list and run it."""
-        tests_cases = []
+        """Check args, set and update a list with all corresponding
+        tests cases lists before run it."""
+        t_cases_lists = []
         if len(sys.argv) == 1:  # no arg -> all tests
-            tests_cases.append(self.all_tests)
+            t_cases_lists.append(self.all_tests)
         else:
             args_dict = vars(self.parse_args())
             for name_tests in self.arg_groups:
                 if args_dict[name_tests[0]]:  # group's name arg -> group tests
-                    tests_cases.append(name_tests[1])
+                    t_cases_lists.append(name_tests[1])
             for test_case in self.all_tests:
                 test_name = test_case.__name__
                 options = args_dict[test_name]
                 if isinstance(options, list):  # test case's name arg
                     if not options:  # no param -> test case's tests
-                        tests_cases.append([test_case])
+                        t_cases_lists.append([test_case])
                     else:  # method name(s) param -> methods's tests
-                        tests_cases.append(([test_case], options))
-        TestCaseRunner(tests_cases).run()
+                        t_cases_lists.append(([test_case], options))
+        TestCaseRunner(t_cases_lists).run()
