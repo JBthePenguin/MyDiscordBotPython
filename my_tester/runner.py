2b8a6a3f077e7e91973062ed4bea175d4ddc3d4c
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-11 17:44:05 +0000

    begin to change run method, ok for full time in console, next send new args to template

--- my_tester/runner.py
+++ my_tester/runner.py
@@ -5,13 +5,10 @@ from datetime import datetime
 
 
 class HtmlTestCaseResult(HtmlTestResult):
-    def __init__(self, stream, descriptions, verbosity):
-        super().__init__(stream, descriptions, verbosity)
 
     def getDescription(self, test):
-        """ Return the test description if not have test name. """
+        """Return the test description with the test method name."""
         return test._testMethodName
-
 
     def _prepare_callback(self, test_info, target_list, verbose_str,
                           short_str):
@@ -20,23 +17,14 @@ class HtmlTestCaseResult(HtmlTestResult):
         target_list.append(test_info)
 
         def callback():
-            """ Print test method outcome to the stream and elapsed time too."""
+            """ Print test method outcome to the stream and elapsed time."""
             test_info.test_finished()
-
             if self.showAll:
                 self.stream.writeln(
                     f"{verbose_str} {str(round(test_info.elapsed_time * 1000, 3))}ms")
             elif self.dots:
                 self.stream.write(short_str)
-
         self.callback = callback
-
-
-    def addSuccess(self, test):
-        """ Called when a test executes successfully. """
-        self._save_output_data()
-        # print(self)
-        self._prepare_callback(self.infoclass(self, test), self.successes, "OK", ".")
 
 
 class TestCaseRunner(HTMLTestRunner):
@@ -65,18 +53,19 @@ class TestCaseRunner(HTMLTestRunner):
             template_args=template_args)
 
     def update_suites_docs(self, test_case):
-        """Return the corresponding tests suite and dict with all tests's docs."""
-        if isinstance(test_case, tuple):
+        """Init a TestSuite for test_case (or methods) and corresponding docs.
+        Update suites property and tests_docs."""
+        if isinstance(test_case, tuple):  # suite with methods
             test_case, m_names = test_case
             suite = TestSuite([test_case(m_name) for m_name in m_names])
-        else:
+        else:  # suite with TestCase
             suite = TestLoader().loadTestsFromTestCase(test_case)
-        tests_docs = {}
-        for test_method in suite._tests:
-            tests_docs[
-                test_method._testMethodName] = test_method._testMethodDoc
-        self.suites.append(suite)
-        self.tests_docs[test_case.__name__] = tests_docs
+        if suite._tests:
+            self.suites.append((test_case.__name__, suite))  # update suites
+            tests_docs = {}  # corresponding docs
+            for test in suite._tests:
+                tests_docs[test._testMethodName] = test._testMethodDoc
+            self.tests_docs[test_case.__name__] = tests_docs  # update docs
 
     def run(self):
         """ Runs the given testcase or testsuite. """
@@ -86,29 +75,23 @@ class TestCaseRunner(HTMLTestRunner):
             self.stream.writeln()
             self.stream.writeln("Running tests... ")
             self.stream.writeln(result.separator2)
-            t_names = []
-            for suite in self.suites:
-                if suite._tests:
-                    t_name = str(suite._tests[0]).split(" ")
-                    t_name = t_name[-1].split('.')[-1][:-1]
-                    t_names.append(t_name)
-            i = 0
             self.start_time = datetime.now()
-            for suite in self.suites:
-                if suite._tests:
-                    self.stream.writeln(f"\n{t_names[i]}\n")
-                    suite(result)
-                    self.stream.writeln(f"\n{result.separator2}")
-                    i += 1
+            for test_case_name, suite in self.suites:
+                self.stream.writeln(f"\n{test_case_name}\n")
+                suite(result)
+                self.stream.writeln(f"\n{result.separator2}")
             stop_time = datetime.now()
             self.time_taken = stop_time - self.start_time
             # print(round(self.time_taken.total_seconds() * 1000, 3))
             result.printErrors()
             self.stream.writeln(result.separator2)
+            full_time = 0
+            for t_r in result.successes + result.failures + result.errors + result.skipped:
+                full_time += t_r.elapsed_time
             run = result.testsRun
             self.stream.writeln("Ran {} test{} in {} ms".format(
                 run, run != 1 and "s" or "",
-                str(round(self.time_taken.total_seconds() * 1000, 3))))
+                str(round(full_time * 1000, 3))))
             self.stream.writeln()
 
             expectedFails = len(result.expectedFailures)
